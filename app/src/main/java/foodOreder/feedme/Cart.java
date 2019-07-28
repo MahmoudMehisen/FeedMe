1341f152cf3906186a5c92fbcec3e57b05534012
mehisen <mahmoud.mehisen@gmail.com>
2019-07-28 22:48:13 +0200

    add open gps

--- app/src/main/java/foodOreder/feedme/Cart.java
+++ app/src/main/java/foodOreder/feedme/Cart.java
@@ -8,7 +8,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -167,6 +169,8 @@ public class Cart extends AppCompatActivity
 
 
         rootLayout = (RelativeLayout)findViewById(R.id.rootLayout);
+
+        turnGPSOn();
 
 
         //Runtime permission
@@ -657,4 +661,17 @@ public class Cart extends AppCompatActivity
         }
 
     }
+
+    private void turnGPSOn(){
+
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE );
+        boolean statusOfGPS = manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if(!statusOfGPS) {
+            Toast.makeText(getApplicationContext(),"Open Gps to get Your location",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+    }
+
 }
