b66abe04fce95ed889cdb1662cf37b9f076ffa30
mehisen <mahmoud.mehisen@gmail.com>
2019-08-01 18:08:04 +0200

    fix waitingDialog bug

--- app/src/main/java/foodOreder/feedme/MainActivity.java
+++ app/src/main/java/foodOreder/feedme/MainActivity.java
@@ -118,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onError(AccountKitError accountKitError) {
-
+                    waitingDailg.dismiss();
                 }
             });
         }
